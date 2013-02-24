aec3267842de13979afab76d64331d845b85f666
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-02-24 11:15:00 +0100

    Add a nice interface to isbn/citaLibro

--- app/views/isbn/citaLibro.blade.php
+++ app/views/isbn/citaLibro.blade.php
@@ -4,4 +4,31 @@
 
 @section('content')
     <h1>Welcome to Fale's Cita libro tool</h1>
+    <form name="module">
+        <p>ISBN</p>
+        <p><input type="text" name="isbn" id="isbn"></p>
+        <input type="button" id="submit" value="Send">
+    </form>
+    <div id="result"></div>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $("#submit").click(function(){
+                var isbn = $("#isbn").val();
+                $.ajax({
+                    type: "GET",
+                    url: "/~fale/isbn/citaLibro/" + isbn,
+                    dataType: "html",
+                    success: function(msg)
+                    {
+                        $("#result").html(msg);
+                    },
+                    error: function()
+                    {
+                        alert("Error, failed call");
+                    }
+                });
+            });
+        });
+    </script>
 @stop
--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Fabio Alessandro Locati">
     <link rel="stylesheet" type="text/css" href="{{ path('bootstrap/css/bootstrap.min.css') }}" />
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <style>
       body {
         padding-top: 60px;
@@ -47,6 +48,5 @@
                  alt="Fork me on GitHub" />
         </a>
     </body>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script type="text/javascript" src="{{ path('bootstrap/js/bootstrap.min.js') }}"></script>
 </html>
