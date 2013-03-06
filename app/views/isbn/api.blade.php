6761f8d2996b8db3d1bae0c5b68a0389f17cd330
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-03-06 10:24:50 +0100

    Improve isbn/api doc

--- app/views/isbn/api.blade.php
+++ app/views/isbn/api.blade.php
@@ -4,6 +4,13 @@
 
 @section('content')
     <h1>ISBN API doc</h1>
-        <h2>ISBN Existance</h2>
-        <h2>Templating</h2>
+        All the functions that can be used with a browser, can be used with APIs.
+        <h3>ISBN Existance</h3>
+            To check if some ISBN code is valid, is enough querying the /~fale/isbn/check/[ISBN NUMBER].
+            <h4>Results</h4>
+            <ul>
+                <li><b>0</b>: ISBN is not registered or invalid</li>
+                <li><b>1</b>: ISBN is valid (but it can be another book ISBN)</li>
+            </ul>
+        <h3>Templating</h3>
 @stop
