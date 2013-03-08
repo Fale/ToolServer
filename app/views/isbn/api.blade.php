1954bf4bb09ad38a71e693a77e75ce3d5e38cf56
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-03-08 01:23:04 +0100

    Improve isbn/api doc

--- app/views/isbn/api.blade.php
+++ app/views/isbn/api.blade.php
@@ -8,9 +8,12 @@
         <h3>ISBN Existance</h3>
             To check if some ISBN code is valid, is enough querying the /~fale/isbn/check/[ISBN NUMBER].
             <h4>Results</h4>
-            <ul>
-                <li><b>0</b>: ISBN is not registered or invalid</li>
-                <li><b>1</b>: ISBN is valid (but it can be another book ISBN)</li>
-            </ul>
+                <ul>
+                    <li><b>0</b>: ISBN is not registered or invalid</li>
+                    <li><b>1</b>: ISBN is valid (but it can be another book ISBN)</li>
+                </ul>
         <h3>Templating</h3>
+            To obtein the template for some ISBN code, is enough querying the /~fale/isbn/cite/[PROJECT]/[ISBN NUMBER].
+            <h4>Results</h4>
+                The template in plain-text will be returned.
 @stop
