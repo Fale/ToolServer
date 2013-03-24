c13d6d261d2fa1ed23d8cf12e5b9e20f8005cee5
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-03-24 10:41:11 +0100

    Fix problems in the user-fixes/accenti file

--- pywikipedia/user-fixes/accenti.py
+++ pywikipedia/user-fixes/accenti.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8  -*-
+
+# TASK: Correggere accenti
+# USE: python replace.py -log -xml:itwiki-latest-pages-articles.xml -namespace:0 -fix:accenti -pt:60
+# STAT: alpha
 
 fixes['accenti'] = {
     'regex': True,
@@ -30,41 +35,4 @@ fixes['accenti'] = {
             'startspace',    # because of code examples
         ],
     }
-}
-    
-        'graphsymb': {
-        'regex': True,
-        'nocase': True,
-        'msg': {
-               'it':u'Correzione dei simboli di punteggiatura'
-              },
-        'replacements': [
-(ur"(\.\s*)E'", ur"\1È"),
-(ur"([^ \t\n\r\f\v0-9]+),([^ \t\n\r\f\v0-9\):]+)", ur"\1, \2"),
-(ur"(\s+),(\s+)", ur", "),
-(ur"([^ \t\n\r\f\v']+)['’`] (\S+)", ur"\1'\2"),
-(ur"([^ \t\n\r\f\v']+)[’`](\S+)", ur"\1'\2"),
-(ur"po['’`](\S+)", ur"po' \1"),
-(ur"Guns N['’`]Roses", ur"Guns N' Roses"),
-(ur"www,", ur"www."),
-(u" po'([^ ,.]{1})", ur" po' \1"),
-(u" mo'([^ ,.]{1})", u" mo' \1"),
-(u" ([Dd])e'([^ ,.]{1})'", ur" \1e' \2"),
-(u" ([Cc])a'([^ ,.]{1})'", ur" \1a' \2"),
-        ],
-        'exceptions': {
-            'inside': [
-                r'\[\[.{2,3}:.*\]\]', #Interwiki
-                r'[Image|File|Immagine]:.*\.[png|jpg|svg]', #Images
-            ],
-            'inside-tags': [
-                'nowiki',
-                'comment',
-                'math',
-                'pre',
-                'source',        # because of code examples
-                'startspace',    # because of code examples
-            ],
-        }
-    },
 }
