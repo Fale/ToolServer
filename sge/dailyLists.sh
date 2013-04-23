0e468afc954ba65ab7b60dcd16b84f362304d7b5
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-04-23 22:58:00 +0200

    Slow down to use less the ToolServer resources

--- sge/dailyLists.sh
+++ sge/dailyLists.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
 
-qcronsub -N ducati $HOME/ToolServer/sge/wrapper/ducati.sh
-qcronsub -N si $HOME/ToolServer/sge/wrapper/si.sh
-qcronsub -N spazi $HOME/ToolServer/sge/wrapper/spazi.sh
-qcronsub -N passati $HOME/ToolServer/sge/wrapper/passati.sh
