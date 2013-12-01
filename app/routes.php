3617b0432a6529c43e8e5cefaafe02ea0f5a7f85
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-12-01 18:36:04 +0100

    Routes should be ToolLabs compatible

--- app/routes.php
+++ app/routes.php
@@ -1,34 +1,39 @@
 <?php
 
-Route::get('/', array(
-	'as' => 'home',
-	function(){ return View::make('home');}
-));
+Route::group(array('prefix' => 'isbn2tpl'), function()
+{
 
-Route::get('api', array(
-	'as' => 'api',
-	function(){ return View::make('api');}
-));
+    Route::get('/', array(
+        'as' => 'home',
+        function(){ return View::make('home');}
+    ));
 
-Route::get('cite', array(
-	'as' => 'cite',
-	function(){ return View::make('cite');}
-));
+    Route::get('api', array(
+        'as' => 'api',
+        function(){ return View::make('api');}
+    ));
 
-Route::get('cite/{project}/{isbn}', array(
-	'uses' => 'IsbnController@cite'
-));
+    Route::get('cite', array(
+        'as' => 'cite',
+        function(){ return View::make('cite');}
+    ));
 
-Route::get('check', array(
-	'as' => 'check',
-	function(){ return View::make('check');}
-));
+    Route::get('cite/{project}/{isbn}', array(
+        'uses' => 'IsbnController@cite'
+    ));
 
-Route::get('check/{isbn}', array(
-	'uses' => 'IsbnController@checkIsbn'
-));
+    Route::get('check', array(
+        'as' => 'check',
+        function(){ return View::make('check');}
+    ));
 
-Route::get('contact', array(
-	'as' => 'contact',
-	function(){ return View::make('contact');}
-));
+    Route::get('check/{isbn}', array(
+        'uses' => 'IsbnController@checkIsbn'
+    ));
+
+    Route::get('contact', array(
+        'as' => 'contact',
+        function(){ return View::make('contact');}
+    ));
+
+});
